06c72794d742d652beb53c06236c136bcaaa2fbb
xxxXilliuMxxx <99077187+xxxXilliuMxxx@users.noreply.github.com>
2026-06-29 20:53:38 +0200

    Update coordinator.py

--- custom_components/fimer/coordinator.py
+++ custom_components/fimer/coordinator.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import logging
-from datetime import timedelta
 
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 from .api import FimerApi
 from .const import DEFAULT_SCAN_INTERVAL
@@ -12,23 +13,31 @@ from .const import DEFAULT_SCAN_INTERVAL
 _LOGGER = logging.getLogger(__name__)
 
 
-class FimerCoordinator(DataUpdateCoordinator):
-    """Coordinator for FIMER data."""
+class FimerCoordinator(DataUpdateCoordinator[dict]):
+    """Coordinator for Aurora Vision."""
 
-    def __init__(self, hass: HomeAssistant, api: FimerApi, plant_id: str):
+    def __init__(
+        self,
+        hass,
+        api: FimerApi,
+    ) -> None:
+
         super().__init__(
             hass,
             _LOGGER,
-            name="Fimer Coordinator",
-            update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
+            name="FIMER Aurora Vision",
+            update_interval=DEFAULT_SCAN_INTERVAL,
         )
 
         self.api = api
-        self.plant_id = plant_id
 
-    async def _async_update_data(self):
+    async def _async_update_data(self) -> dict:
+        """Fetch data from Aurora Vision."""
+
         try:
-            data = await self.api.get_plant_data(self.plant_id)
-            return data
+            return await self.api.get_data()
+
         except Exception as err:
-            raise UpdateFailed(f"Fimer update failed: {err}") from err
+            raise UpdateFailed(
+                f"Failed updating Aurora Vision: {err}"
+            ) from err
