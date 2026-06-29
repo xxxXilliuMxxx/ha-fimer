35c484ca949fbafed54f014dd70573e37f44af0d
xxxXilliuMxxx <99077187+xxxXilliuMxxx@users.noreply.github.com>
2026-06-29 20:28:51 +0200

    Update const.py

--- custom_components/fimer/const.py
+++ custom_components/fimer/const.py
@@ -1,14 +1,33 @@
+"""Constants for the FIMER Aurora Vision integration."""
+
+from __future__ import annotations
+
+from datetime import timedelta
+
 DOMAIN = "fimer"
 
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_PLANT_ID = "plant_id"
 
-DEFAULT_SCAN_INTERVAL = 60
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
 
 MANUFACTURER = "FIMER"
 MODEL = "Aurora Vision"
 
-API_TIMEOUT = 20
+ATTR_GENERATION_POWER = "generation_power"
+ATTR_GRID_POWER = "grid_power"
+ATTR_BATTERY_POWER = "battery_power"
+
+ATTR_GENERATION_ENERGY_TODAY = "generation_energy_today"
+ATTR_GENERATION_ENERGY_TOTAL = "generation_energy_total"
+
+ATTR_GRID_IMPORT_TODAY = "grid_import_today"
+ATTR_GRID_EXPORT_TODAY = "grid_export_today"
+
+ATTR_BATTERY_CHARGE_TODAY = "battery_charge_today"
+ATTR_BATTERY_DISCHARGE_TODAY = "battery_discharge_today"
+
+ATTR_BATTERY_SOC = "battery_soc"
 
 ATTR_LAST_UPDATE = "last_update"
