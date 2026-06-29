5b8e9dea455c42572b543185a6a4fac90c0d474d
xxxXilliuMxxx <99077187+xxxXilliuMxxx@users.noreply.github.com>
2026-06-29 20:58:05 +0200

    Update config_flow.py

--- custom_components/fimer/config_flow.py
+++ custom_components/fimer/config_flow.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.core import callback
 
 from .const import (
     CONF_PASSWORD,
@@ -12,31 +13,72 @@ from .const import (
 )
 
 
-class FimerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class FimerConfigFlow(
+    config_entries.ConfigFlow,
+    domain=DOMAIN,
+):
+    """Handle a config flow for FIMER."""
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(
+        self,
+        user_input=None,
+    ):
 
         errors = {}
 
         if user_input is not None:
+
+            await self.async_set_unique_id(
+                user_input[CONF_PLANT_ID]
+            )
+
+            self._abort_if_unique_id_configured()
 
             return self.async_create_entry(
                 title=f"Plant {user_input[CONF_PLANT_ID]}",
                 data=user_input,
             )
 
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): str,
-                vol.Required(CONF_PASSWORD): str,
-                vol.Required(CONF_PLANT_ID): str,
-            }
-        )
-
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Required(CONF_PLANT_ID): str,
+                }
+            ),
             errors=errors,
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return FimerOptionsFlow(config_entry)
+
+
+class FimerOptionsFlow(
+    config_entries.OptionsFlow,
+):
+    """Handle FIMER options."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self,
+        user_input=None,
+    ):
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({}),
         )
